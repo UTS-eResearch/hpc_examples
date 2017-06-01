c7eabf1491f2d12541012a15f7100bfbf8aafa6c
Mike Lake <mike.lake@uts.edu.au>
2017-06-01 12:32:57 +1000

    Lots of changes I know not what.

--- cuda/compile.sh
+++ cuda/compile.sh
@@ -2,7 +2,7 @@
 
 # We need to set a correct PATH and LD_LIBRARY_PATH for the compilation. 
 # Note you must source the script, not run it. 
-source ../set_cuda_env.sh
+source ./set_cuda_env.sh
 
 make clean
 
--- gpu_access.sh
+++ gpu_access.sh
@@ -5,23 +5,25 @@
 # See "Interactive-batch Jobs" in the "PBS User Guide".
 #
 # For interactive use you must submit this job with -I 
-# qsub -I gpu_access.sh
+#
+#   qsub -I gpu_access.sh
+#
 
 # maximums: ncps=48, walltime=200:00:00, mem=256GB/48cores
 # but for interactive use we use the minimums.
 #PBS -l ncpus=1
 #PBS -l mem=5GB
-#PBS -q smallq
+#PBS -q teachingq
 
 # Set your interactive wall time limit.
-#PBS -l walltime=00:15:00
+#PBS -l walltime=00:01:00
 
 # Set your email.
 #PBS -m abe 
 #PBS -M Mike.Lake@uts.edu.au
 
 # Set the node to start an interactive session on.
-####PBS -l host=hpcinode1
+###PBS -l host=hpcinode1
 #PBS -l host=ermdc21
 
 # Note: all other commands won't work.
