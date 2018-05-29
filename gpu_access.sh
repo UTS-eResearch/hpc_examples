fb678e0f74fa69059bb13321d802df9182ffdee6
950654 <Mike.Lake@uts.edu.au>
2018-05-29 11:38:07 +1000

    minor

--- gpu_access.sh
+++ gpu_access.sh
@@ -13,7 +13,7 @@
 # but for interactive use we use the minimums.
 #PBS -l ncpus=1
 #PBS -l mem=5GB
-#PBS -q teachingq
+#PBS -q smallq
 
 # Set your interactive wall time limit.
 #PBS -l walltime=00:01:00
@@ -23,7 +23,6 @@
 #PBS -M Mike.Lake@uts.edu.au
 
 # Set the node to start an interactive session on.
-###PBS -l host=hpcinode1
 #PBS -l host=ermdc21
 
 # Note: all other commands won't work.
