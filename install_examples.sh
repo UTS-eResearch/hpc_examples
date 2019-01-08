d1dab83dc35f011591360c02e14bc16bc702d141
Mike Lake <Mike.Lake@uts.edu.au>
2019-01-08 15:17:19 +1100

    Added install script

---
+++ install_examples.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Installs example files to the cluster.
+
+dest="/shared/eresearch/pbs_job_examples"
+
+echo ''
+echo "=== Install Examples to Cluster ==="
+echo ''
+
+echo '------ GIT Status ------'
+git st
+
+# Ask user what to do.
+echo
+echo 'Install these files?' 
+echo "Press (y/Y) to delete or press any other key to exit."
+read REPLY
+if [ -z $REPLY ]; then
+   exit 0
+fi
+
+# If user has NOT selected y/Y then exit.
+if [ $REPLY != "y" ] && [ $REPLY != "Y" ]; then
+    exit 0
+fi
+
+# Otherwise we continue.
+if [ ! -d $dest ]; then
+    echo "Are we on the cluster"
+    exit 0
+fi
+
+# Install primes examples
+cp primes/README.txt $dest/primes/
+cp primes/*.sh       $dest/primes/
+cp primes/*.py       $dest/primes/
+ 
