d2e8f777be314c834e2ec68ce1c749afe0e6af88
Mike Lake <Mike.Lake@uts.edu.au>
2019-09-26 11:21:06 +1000

    Added install

---
+++ matlab/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install the example files.
+
+dest="/shared/eresearch/pbs_job_examples/matlab/"
+mkdir -p $dest
+
+cp README.txt $dest
+cp *.m $dest
+cp *.sh $dest
+
+# Modify and copy the submission scripts.
+#pbs_scripts=' test_single_job.sh test_parallel_job.sh girkos_job.sh'
+#
+#for pbs_script in $pbs_scripts; do
+#    cat $pbs_script | sed 's/Mike.Lake@uts.edu.au/999777@uts.edu.au/' > $dest/$pbs_script
+#done
+
