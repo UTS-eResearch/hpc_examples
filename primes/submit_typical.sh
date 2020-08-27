9af8e7b0753938b586f2fc77fae5bdf337cd718a
Mike Lake <Mike.Lake@uts.edu.au>
2020-08-27 10:37:50 +1000

    Updated

--- primes/submit_typical.sh
+++ primes/submit_typical.sh
@@ -7,6 +7,7 @@
 #   In this example we have used a staff/student number of 999777.
 #   You will need to change all instances of this to your own 
 #   staff/student number before you try and run this script. 
+#   See the "PBS -M" below.
 #
 # Run this as qsub scriptname
 #
@@ -14,7 +15,7 @@
 
 ##################
 # Set PBS Commands 
-#################
+##################
 
 # Set the resource requirements; 1 CPU, 5 GB memory and 5 minutes wall time.
 #PBS -N test
@@ -35,27 +36,36 @@
 # Setup any input files for the run
 ###################################
   
-# If you have any input files then create your own directory under the 
-# "/scratch/" directory and copy your input files to there. 
-# Note: This scratch directory will be on the node that the job has 
-# been submitted to by PBS. 
+# If you have any large input or output files then they need to be read and written 
+# to the nodes local /scratch directory. So you need to:
+# 1. create this /scratch directory
+# 2. copy your input files to there
+# 3. copy your output files from there, back to your home directory.
+# Note: This scratch directory will only be on the node that the job has 
+# been assigned to by PBS. 
 
-# The shell parameters which are useful are:
+# There are some shell parameters which are useful for creating a unique 
+# scratch directory. These are:
 #   USER which is the name of the logged in user (e.g. 999777), 
 #   PBS_JOBID which is the job ID of this PBS job (e.g. 184327.hpcnode0) and 
-#   PBS_JOBID%.* is a bash parameter expansion. See "Parameter Expansion" in "man bash".
-# 
+#   PBS_JOBID%.* is a bash parameter expansion. See "Parameter Expansion" 
+#     and "Remove matching suffix pattern" in "man bash".
+#   
+# The PBS_JOBID%.* parameter expansion: 
+# This removes the matching suffix pattern, in this case .* i.e. everyting after the PBS_JOBID.
 # For instance if PBS_JOBID is 184327.hpcnode0 then PBS_JOBID%.* will be just 184327.
 # Hence the scratch directory created will be /scratch/999777_184327
 # This will be unique for every PBS job you submit.
 
+# Create your /scratch directory:
 SCRATCH="/scratch/${USER}_${PBS_JOBID%.*}"
 mkdir ${SCRATCH}
 
 # Change to the PBS working directory where qsub was started from.
-# Copy your input files from there to the scratch directory you just created.
 # The shell parameter PBS_O_WORKDIR is the working directory where this job was started from.
 cd ${PBS_O_WORKDIR}
+
+# Copy your input files from there to the scratch directory you created above.
 if [ -f primes.txt ]; then 
     cp primes.txt ${SCRATCH}
 fi
