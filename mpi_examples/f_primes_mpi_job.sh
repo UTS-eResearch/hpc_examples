b56ac1dcf68cf4ba88ae9e20ceacbd585255d509
Mike Lake <Mike.Lake@uts.edu.au>
2020-09-02 11:42:53 +1000

    Renamed

--- mpi_examples/f_primes_mpi_job.sh
+++ mpi_examples/f_primes_mpi_job.sh
@@ -29,10 +29,10 @@ cd $PBS_O_WORKDIR
 cat $PBS_NODEFILE
 
 # OPTION 1
-#mpiexec -np 4 f_primes_with_mpi
+#mpiexec -np 4 f_primes_mpi
 
 # OPTION 2
-mpiexec -np 8 f_primes_with_mpi
+mpiexec -np 8 f_primes_mpi
 
 ## #source env.sh
 #PATH=/opt/pbs/bin:$PATH
