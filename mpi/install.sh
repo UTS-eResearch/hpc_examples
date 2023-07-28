78825edfaba40148640792fe627ed5c1c0fd2eec
Mike Lake <Mike.Lake@uts.edu.au>
2023-07-28 16:22:12 +1000

    Added install script.

---
+++ mpi/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+dest='/shared/eresearch/pbs_job_examples/mpi/'
+mkdir -p $dest
+
+#pbs_script='primes_job.sh'
+# Modify and copy the submission script.
+#cat $pbs_script | sed 's/Mike.Lake@uts.edu.au/999777@uts.edu.au/' > temp
+#cat temp | sed 's/\/shared\/homes\/mlake/$HOME/' > temp.sh
+#cp temp.sh $dest/$pbs_script
+#rm -f temp.sh
+#rm -f temp
+
+# Copy the README files.
+cp README.md $dest 
+cp README.yaml $dest 
+
+# Copy the program files.
+cp clean.sh $dest
+cp compile_all.sh $dest
+cp *.f90 $dest
+cp submit_primes_serial.sh $dest
+cp submit_primes_mpi.sh $dest
+
