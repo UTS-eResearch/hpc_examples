c3778062beed4c0266cb47125c9fc0ec1700d9ac
Mike Lake <Mike.Lake@uts.edu.au>
2025-02-28 09:57:11 +1100

    Minor change

--- job_arrays/clean.sh
+++ job_arrays/clean.sh
@@ -6,3 +6,7 @@ rm -f *.o*[0-9]
 rm -f primes_[0-9]*.txt
 rm -f progress.txt
 
+# We also remove any file like this which would occur
+# if there is no value for the $PBS_ARRAY_INDEX
+rm -f primes_*.txt
+
