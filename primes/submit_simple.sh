e70b6b3c3b33f715cee47a5a13e2608e7c3e8aab
Mike Lake <Mike.Lake@uts.edu.au>
2025-04-01 14:47:25 +1100

    Remove blank spaces at end

--- primes/submit_simple.sh
+++ primes/submit_simple.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Example of the simplest qsub submission script for the primes program.
-# Please read submit_typical.sh which is more typical of what your 
+# Please read submit_typical.sh which is more typical of what your
 # submission script should be like.
 #
-# Important: 
-#   If you wish this script to email you when the job has started and ended 
-#   then you will need to remove the extra # from the start of the two lines 
+# Important:
+#   If you wish this script to email you when the job has started and ended
+#   then you will need to remove the extra # from the start of the two lines
 #   ##PBS -m and ##PBS -M and edit the latter to your own email.
-#    
+#
 # Run this as: qsub submit_simple.sh
 #
 # Author: Mike Lake
 
 ##################
-# PBS commands must come at the top of this script, 
+# PBS commands must come at the top of this script,
 # before any other commands.
 #################
 
@@ -22,15 +22,15 @@
 #PBS -N primes
 #PBS -l ncpus=1
 #PBS -l mem=5GB
-#PBS -l walltime=00:05:00 
+#PBS -l walltime=00:05:00
 
-# Send email on abort, begin and end. 
+# Send email on abort, begin and end.
 # CHANGE the below to your email and remove the extra # from the start of the next two lines.
-##PBS -m abe 
+##PBS -m abe
 ##PBS -M your.email@uts.edu.au
 
-# In this example there are no input files. 
-# You just change directory to where your program is and run it. 
+# In this example there are no input files.
+# You just change directory to where your program is and run it.
 # Your output file will be in that directory.
 # The shell parameter PBS_O_WORKDIR is the working directory where this job was started from.
 
--- primes/submit_typical.sh
+++ primes/submit_typical.sh
@@ -3,17 +3,17 @@
 # Example of a typical qsub submission script for the primes program.
 # This uses the /scratch directory which is what you should be using.
 #
-# Important: 
-#   If you wish this script to email you when the job has started and ended 
-#   then you will need to remove the extra # from the start of the two lines 
+# Important:
+#   If you wish this script to email you when the job has started and ended
+#   then you will need to remove the extra # from the start of the two lines
 #   ##PBS -m and ##PBS -M and edit the latter to your own email.
-#    
+#
 # Run this as: qsub submit_typical.sh
 #
 # Author: Mike Lake
 
 ##################
-# Set PBS Commands 
+# Set PBS Commands
 ##################
 
 # Set the resource requirements; 1 CPU, 5 GB memory and 5 minutes wall time.
@@ -21,34 +21,34 @@
 #PBS -N primes
 #PBS -l ncpus=1
 #PBS -l mem=5GB
-#PBS -l walltime=00:05:00 
+#PBS -l walltime=00:05:00
 
-# Send email on abort, begin and end. 
+# Send email on abort, begin and end.
 # CHANGE your.email to your email and remove the extra # from the start of the next two lines.
 # More than one # characters before a PBS line acts to comment out that line.
-##PBS -m abe 
+##PBS -m abe
 ##PBS -M your.email@uts.edu.au
 
 ###################################
 # Setup any input files for the run
 ###################################
-  
-# If you have any large input or output files then they need to be read and written 
+
+# If you have any large input or output files then they need to be read and written
 # to the nodes local /scratch directory. So you need to:
 # 1. create this /scratch directory
 # 2. copy your input files to there
 # 3. copy your output files from there, back to your home directory.
-# Note: This scratch directory will only be on the node that the job has 
-# been assigned to by PBS. 
+# Note: This scratch directory will only be on the node that the job has
+# been assigned to by PBS.
 
-# There are some shell parameters which are useful for creating a unique 
+# There are some shell parameters which are useful for creating a unique
 # scratch directory. These are:
-#   USER which is the name of the logged in user (e.g. u999777), 
-#   PBS_JOBID which is the job ID of this PBS job (e.g. 184327.hpcnode0) and 
-#   PBS_JOBID%.* is a bash parameter expansion. See "Parameter Expansion" 
+#   USER which is the name of the logged in user (e.g. u999777),
+#   PBS_JOBID which is the job ID of this PBS job (e.g. 184327.hpcnode0) and
+#   PBS_JOBID%.* is a bash parameter expansion. See "Parameter Expansion"
 #     and "Remove matching suffix pattern" in "man bash".
-#   
-# The PBS_JOBID%.* parameter expansion: 
+#
+# The PBS_JOBID%.* parameter expansion:
 # This removes the matching suffix pattern, in this case .* i.e. everyting after the PBS_JOBID.
 # For instance if PBS_JOBID is 184327.hpcnode0 then PBS_JOBID%.* will be just 184327.
 # Hence the scratch directory created will be /scratch/u999777_184327
@@ -59,13 +59,13 @@ SCRATCH="/scratch/${USER}_${PBS_JOBID%.*}"
 mkdir ${SCRATCH}
 
 # Change to the PBS working directory where qsub was started from.
-# The shell parameter PBS_O_WORKDIR is the working directory where this job 
+# The shell parameter PBS_O_WORKDIR is the working directory where this job
 # was started from. We do this because this bash script has started a new
 # shell and so this point your working directory is your home directory.
 cd ${PBS_O_WORKDIR}
 
 # Copy your input files from there to the scratch directory you created above.
-if [ -f primes.txt ]; then 
+if [ -f primes.txt ]; then
     cp primes.txt ${SCRATCH}
 fi
 
@@ -73,7 +73,7 @@ fi
 # Start the Job
 ###############
 
-# Change to the scratch directory where you copied your input 
+# Change to the scratch directory where you copied your input
 # files to before you start. Then run the primes program.
 cd ${SCRATCH}
 ${PBS_O_WORKDIR}/primes.py
