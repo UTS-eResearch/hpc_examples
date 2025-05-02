3ddc16c476f76ae5afeab0682cf674e835c68e91
speleolinux <mikel@speleonics.com.au>
2025-05-02 12:45:37 +1000

    Changes to make it similar to python script.

--- primes_using_R/submit.sh
+++ primes_using_R/submit.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-#PBS -N Test_R
+# Run this as: qsub submit.sh
+#
+# Author: Mike Lake
+
+# Set the resource requirements; 1 CPU, 5 GB memory and 5 minutes wall time.
+#PBS -N primes
 #PBS -l ncpus=1
 #PBS -l mem=5GB
-#PBS -l walltime=00:10:00 
-##PBS -q testq
+#PBS -l walltime=00:05:00
 
 # Send email on abort, begin and end.
-#PBS -m abe 
-#PBS -M Mike.Lake@uts.edu.au
+# CHANGE the below to your email and remove the extra # from the start of the next two lines.
+##PBS -m abe
+##PBS -M your.email@uts.edu.au
 
-# Change to the PBS working directory where qsub was started from.
+# In this example there are no input files.
+# You just change directory to where your program is and run it.
+# Your output file will be in that directory.
+# The shell parameter PBS_O_WORKDIR is the working directory where this job was started from.
+
 cd ${PBS_O_WORKDIR}
-
-# Run the primes program.
 Rscript primes.r 100000 200000
 
